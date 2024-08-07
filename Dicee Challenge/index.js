17becfa870f3df67b70d44974b227f2b4e619808
LekhNayak <lekhnayak03@gmail.com>
2024-08-07 19:08:26 +0530

    kusu lodu

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,29 +1,23 @@
+// Generate random numbers for both dice
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1-6
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
+// Create the image file names
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+// Update the image sources
+document.querySelectorAll("img")[0].src = randomImageSource1;
+document.querySelectorAll("img")[1].src = randomImageSource2;
 
-
-//If player 1 wins
+// Determine the winner and update the heading text
+var resultText;
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  resultText = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  resultText = "Player 2 Wins!";
+} else {
+  resultText = "Draw!";
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!";
-}
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
+
+document.querySelector("h1").innerHTML = resultText;
